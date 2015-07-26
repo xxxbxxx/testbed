35cd0d608782fd5753dadff89393858398d1dbc2
xavier <xavierb@gmail.com>
2015-07-26 17:26:19 +0200

    Load resources

--- main.cpp
+++ main.cpp
@@ -1,11 +1,107 @@
 // testing tools for openal
 
-#include <imgui.h>
-#include "imgui_impl_sdl.h"
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+#include <AL/al.h>
+#include <AL/alext.h>
+
+#include <imgui.h>
+#include "imgui_impl_sdl.h"
+
+//#define DResourcesRoot "./data/"
+#define DResourcesRoot "/home/shared/src/xbx/testbed-openal/data/"
+
+#define ERR(...)    fprintf(stderr, __VA_ARGS__)
+
+
+// -------------------  LoadSound -------------------------
+struct SWavData {
+	ALenum format;
+	ALsizei size;
+	ALsizei freq;
+	const ALvoid *data;
+};
+
+bool LoadSound(const char* name, SWavData& _Wav)
+{
+	SDL_AudioSpec wav_spec;
+	Uint32 wav_length;
+	Uint8 *wav_buffer;
+	if (SDL_LoadWAV(name, &wav_spec, &wav_buffer, &wav_length) == NULL) {
+		ERR("LoadSound(%s): SDL_LoadWAV failed: %s\n", name, SDL_GetError());
+		return false;
+	}
+
+	ALenum format = 0;
+	if (wav_spec.channels == 1) {
+		switch(wav_spec.format) {
+		case AUDIO_U8:			format = AL_FORMAT_MONO8; break;
+		case AUDIO_S16LSB:		format = AL_FORMAT_MONO16; break;
+		case AUDIO_F32LSB:		format = AL_FORMAT_MONO_FLOAT32; break;
+		}
+	} else if (wav_spec.channels == 2) {
+		switch(wav_spec.format) {
+		case AUDIO_U8:			format = AL_FORMAT_STEREO8; break;
+		case AUDIO_S16LSB:		format = AL_FORMAT_STEREO16; break;
+		case AUDIO_F32LSB:		format = AL_FORMAT_STEREO_FLOAT32; break;
+		}
+	}
+
+	if (format == 0) {
+		// TODO: if needed, more channels / other formats.
+		ERR("LoadSound(%s): Unsupported format (TODO): 0x%X\n", name, wav_spec.format);
+		SDL_FreeWAV(wav_buffer);
+		return false;
+	}
+
+	_Wav.data = wav_buffer;
+	_Wav.size = wav_length;
+	_Wav.freq = wav_spec.freq;
+	_Wav.format = format;
+	return true;
+}
+
+void FreeSound(SWavData& _Wav)
+{
+	if (_Wav.data)
+		SDL_FreeWAV((Uint8*)_Wav.data);
+	_Wav.data = NULL;
+}
+
+
+
+// ------------------- Program resources -------------------------
+
+struct SResources
+{
+	SWavData wav_mono;
+	SWavData wav_stereo;
+};
+
+bool LoadResources(SResources& _Res)
+{
+	bool ok;
+	ok = LoadSound(DResourcesRoot "mono.wav", _Res.wav_mono);
+	if (!ok)
+		return false;
+
+	ok = LoadSound(DResourcesRoot "stereo.wav", _Res.wav_stereo);
+	if (!ok)
+		return false;
+
+	return true;
+}
+
+void FreeResources(SResources& _Res)
+{
+	FreeSound(_Res.wav_mono);
+	FreeSound(_Res.wav_stereo);
+}
+
+
+// ------------------- Main -------------------------
 
 int main(int, char**)
 {
@@ -26,6 +122,15 @@ int main(int, char**)
 
 	// Setup ImGui binding
 	ImGui_ImplSdl_Init(sdl_window);
+
+	// Load resource
+	SResources Resources;
+	bool ok = LoadResources(Resources);
+	if (!ok) {
+		ERR("Could not load all program resource.\n");
+		return 1;
+	}
+
 
 	ImVec4 clear_color = ImColor(114, 144, 154);
 
@@ -62,6 +167,8 @@ int main(int, char**)
 		ImGui::Render();
 		SDL_GL_SwapWindow(sdl_window);
 	}
+
+	FreeResources(Resources);
 
 	// Cleanup
 	ImGui_ImplSdl_Shutdown();
